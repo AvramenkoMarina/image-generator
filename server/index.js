0dfa07aa7a5c6af37ef30e01cf98971c7db5673e
Maryna Avramenko <maryna.av@inforce.digital>
2025-12-09 20:52:14 +0200

    change root

--- server/index.js
+++ server/index.js
@@ -54,7 +54,7 @@ const __dirname = path.dirname(__filename);
 const clientBuildPath = path.join(__dirname, "../client/dist");
 app.use(express.static(clientBuildPath));
 
-app.get("/*", (req, res) => {
+app.get(/.*/, (req, res) => {
   res.sendFile(path.join(clientBuildPath, "index.html"));
 });
 
