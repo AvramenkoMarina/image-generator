c31df019d4d9732e05ac442631288343a803ad60
Maryna Avramenko <maryna.av@inforce.digital>
2025-12-09 20:42:37 +0200

    change index.js

--- server/index.js
+++ server/index.js
@@ -51,10 +51,11 @@ app.post("/generate", async (req, res) => {
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-app.use(express.static(path.join(__dirname, "../client/dist")));
+const clientBuildPath = path.join(__dirname, "../client/dist");
+app.use(express.static(clientBuildPath));
 
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../client/dist", "index.html"));
+app.get("/*", (req, res) => {
+  res.sendFile(path.join(clientBuildPath, "index.html"));
 });
 
 const PORT = process.env.PORT || 5001;
